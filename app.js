c9a65347716fc6d18bcfac6ec377cf3da596b3a2
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-23 19:34:48 -0800

    Use array syntax for delcaring controllers

--- app.js
+++ app.js
@@ -2,21 +2,21 @@
 
 var swapiApp = angular.module('swapiApp', ["ui.router"])
 
-swapiApp.controller('PlanetsCtrl', function($http) {
+swapiApp.controller('PlanetsCtrl', ['$http', function($http) {
   $http.get('http://swapi.co/api/planets/?format=json')
   .then(res => this.list = res.data.results,
         err => console.log('GET ERROR:', err));
 
   this.extractId = url => url.split('/').slice(-2)[0];
-});
+}]);
 
-swapiApp.controller('ResidentCtrl', function($state, $stateParams, $http) {
+swapiApp.controller('ResidentCtrl', ['$state', '$stateParams', '$http', function($state, $stateParams, $http) {
   this.id = $stateParams.id;
 
   $http.get('http://swapi.co/api/people/' + this.id + '/?format=json')
   .then(res => this.character = res.data,
         err => console.log('GET ERROR:', err));
-});
+}]);
 
 // swapiApp.service();
 
