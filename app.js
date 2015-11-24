f19e1c3880941b4ca9e69ad70db9208b47f88d36
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-23 19:05:36 -0800

    Clean up

--- app.js
+++ app.js
@@ -2,19 +2,6 @@
 
 var swapiApp = angular.module('swapiApp', ["ui.router"])
 
-swapiApp.controller('IndexCtrl', function($state, $stateParams, $http) {
-  this.id = $stateParams.id;
-
-  $http.get('http://swapi.co/api/people/' + this.id + '/?format=json')
-  .then(res => this.character = res.data,
-        err => console.log('GET ERROR:', err));
-
-  this.doSomething = function() {
-    console.log('something');
-    this.character.name += '+';
-  }
-});
-
 swapiApp.controller('PlanetsCtrl', function($http) {
   $http.get('http://swapi.co/api/planets/?format=json')
   .then(res => this.list = res.data.results,
@@ -35,7 +22,7 @@ swapiApp.controller('ResidentCtrl', function($state, $stateParams, $http) {
 
 swapiApp.config(function($stateProvider, $urlRouterProvider) {
   
-  // For any unmatched url, send to /planets
+  // for any unmatched url, send to /planets
   $urlRouterProvider.otherwise("/planets")
   
   $stateProvider
@@ -50,37 +37,4 @@ swapiApp.config(function($stateProvider, $urlRouterProvider) {
       controller: 'ResidentCtrl as r'
     })
 
-    .state('whocares', {
-      url: "",
-      template: "42"
-    })
-    .state('users', {
-      url: "/user/:id",
-      templateUrl: "42.html",
-      controller: "IndexCtrl as i" // alias the controller
-    })
-
-    .state('route1', {
-      url: "/route1",
-      templateUrl: "route1.html"
-    })
-      .state('route1.list', {
-        url: "/list",
-        templateUrl: "route1.list.html",
-        controller: function($scope) {
-          $scope.items = ["A", "List", "Of", "Items"];
-        }
-      })
-      
-    .state('route2', {
-      url: "/route2",
-      templateUrl: "route2.html"
-    })
-      .state('route2.list', {
-        url: "/list",
-        templateUrl: "route2.list.html",
-        controller: function($scope) {
-          $scope.things = ["A", "Different", "List"];
-        }
-      })
 })
